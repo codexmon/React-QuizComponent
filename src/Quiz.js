33f4aee49f938b523502035694461e31cd1d00ee
rev <rev@anniecannons.com>
2018-12-06 20:08:38 -0800

    module 4 sans last step

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import QuizQuestion from './QuizQuestion.js';
+import QuizEnd from './QuizEnd.js';
 
 let quizData = require('./quiz_data.json');
 
@@ -11,6 +12,7 @@ class Quiz extends Component {
     }
 
     render(){
+        const isQuizEnd = ((this.state.quiz_question -1) === quizData.quiz_questions.length)
         return (
             <div>
                 {/* <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div> 
@@ -18,7 +20,8 @@ class Quiz extends Component {
                 Inside of this div with a className of QuizQuestion, we are accessing the first element in the quiz_questions property of the quizData object, and displaying the instruction_text value.
 
                 This div can be replaced by the QuizQuestion component on the next line*/}
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1] } />
+                {isQuizEnd ? <QuizEnd /> :
+                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1] } />}
                 {/* In that <QuizQuestion /> component, add a prop named quiz_question that passes the value of the quizData.quiz_questions array at the state's quiz_position minus 1. This will send data for the first question over to the QuizQuestion component. */}
 
             </div>
