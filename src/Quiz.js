5d592c060b544d6d242589b4012cb4e540908e15
rev <rev@anniecannons.com>
2018-12-06 20:27:11 -0800

    last step of module 4

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,22 +12,21 @@ class Quiz extends Component {
     }
 
     render(){
-        const isQuizEnd = false
+        const isQuizEnd = ((this.state.quiz_question -1)===quizData.quiz_questions.length)
         return (
             <div>
-                {/* <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div> 
-
-                Inside of this div with a className of QuizQuestion, we are accessing the first element in the quiz_questions property of the quizData object, and displaying the instruction_text value.
-
-                This div can be replaced by the QuizQuestion component on the next line*/}
                 {isQuizEnd ? <QuizEnd /> : 
                 <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />}
-                {/* In that <QuizQuestion /> component, add a prop named quiz_question that passes the value of the quizData.quiz_questions array at the state's quiz_position minus 1. This will send data for the first question over to the QuizQuestion component. */}
 
             </div>
         )
         
     }
 }
+                {/* <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div> 
 
+                Inside of this div with a className of QuizQuestion, we are accessing the first element in the quiz_questions property of the quizData object, and displaying the instruction_text value.
+
+                This div can be replaced by the QuizQuestion component on the next line*/}
+                {/* In that <QuizQuestion /> component, add a prop named quiz_question that passes the value of the quizData.quiz_questions array at the state's quiz_position minus 1. This will send data for the first question over to the QuizQuestion component. */}
 export default Quiz;
